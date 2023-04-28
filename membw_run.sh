e6389813345119b810b4164c40bd593f5b606861
Neel Patel <n869p538@pollux.ittc.ku.edu>
2023-04-28 17:22:21 -0500

    19 stressor core lbm_run config

--- membw_run.sh
+++ membw_run.sh
@@ -11,16 +11,20 @@ STRESS_CORES=( "1" "5" "9" "14" "19" )
 STRESS_CORES=( "0" "1" "2" "5" "7" "9" "14" "19" "25" )
 STRESS_CORES=( "0" "1" "2" "5" "7" "9" "14" "17" "19" )
 STRESS_CORES=( `seq 0 19` )
-STRESS_CORES=( "1" )
+STRESS_CORES=( "9" )
+STRESS_CORES=( "0" "1" "2" "5" "7" "9" "14" "17" "19" )
+STRESS_CORES=( "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "11" "12" "13" "14" "15" "16" "17" "18" "19" )
 
 SPEC_CORES=( 10 )
 BENCHS=( "lbm_s" )
 
-test_n=lbmx1_maxmembw
+test_n=lbm_x${#SPEC_CORES[@]}_membw_x${#STRESS_CORES[@]}
 PQOS_OUTPUT=$test_n/pqos/
 SPEC_TEST_OUTPUT=$test_n/spec/
 mkdir -p $PQOS_OUTPUT
 mkdir -p $SPEC_TEST_OUTPUT
+
+build_all
 
 # start stressors
 taskset -c $( echo ${STRESS_CORES[@]} | sed -e 's/ /,/g' -e 's/,$//g') stress-ng --memrate ${#STRESS_CORES[@]} --memrate-wr 50000 --memrate-rd 50000 &
